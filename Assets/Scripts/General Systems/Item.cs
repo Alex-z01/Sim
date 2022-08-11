61cc234aa973c896051eea4840ceef821dcfb6c5
Alex <alexzchicken@gmail.com>
2022-08-11 00:48:22 -0400

    General item script

---
+++ Assets/Scripts/General Systems/Item.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string Item_Name, Item_Description;
+    public int Buy_Price, Sell_Price;
+    public int Level_Req;
+
+    public Sprite Item_Icon;
+}
