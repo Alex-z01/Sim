e1546f8cb80ca9844dd1b58a4be7f2b0f4cab926
Alex <alexzchicken@gmail.com>
2022-08-11 21:42:14 -0400

    inventory slot

---
+++ Assets/Scripts/General Systems/Inventory/ItemSlot.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public Item attachedItem;
+    public Image itemIcon;
+    public Text itemCount;
+
+    public void SetCountText(int count)
+    {
+        itemCount.text = $"{count}";
+    }
+
+    public void SetIcon(Sprite icon)
+    {
+        itemIcon.sprite = icon;
+    }
+
+    public void SetAttachedItem(Item item)
+    {
+        attachedItem = item;
+    }
+
+    public void UseItem()
+    {
+        if(attachedItem != null)
+        {
+            print(attachedItem.Item_Name);
+            if (attachedItem.GetType() == typeof(Equippable))
+            {
+                var equippable = (Equippable)attachedItem;
+
+                attachedItem = null;
+                itemIcon = null;
+                GameManager.instance.Player.GetComponent<PlayerInventory>().EquipGear(equippable);
+            }
+        }
+    }
+}
